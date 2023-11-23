65b0eaf082cf54c2ad86ad9811da4b54f5e78d2d
brian-ambani <brainambani1@gmail.com>
2023-11-24 02:43:59 +0300

    updated the folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages and components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Workout Buddy</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
