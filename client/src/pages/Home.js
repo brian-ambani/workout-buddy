8d5d5c1f6d8278fab737b9da9c5ec927846b6905
brian-ambani <brainambani1@gmail.com>
2023-11-24 03:12:18 +0300

    updated the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
     </div>
   );
 };
